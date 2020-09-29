3e51ee61361559f2fbeab278da29b60402af3608
luxidea <contact@luxidea.net>
2020-09-30 01:02:39 +0900

    no message

--- WacomTablet.dpr
+++ WacomTablet.dpr
@@ -4,11 +4,11 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   Main in 'Main.pas' {Form1},
-  WINTAB in '_LIBRARY\WINTAB.pas',
   LUX.FMX.Messaging.Win in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Messaging.Win.pas',
   LUX.WinTab in '_LIBRARY\LUXOPHIA\LUX.PenTablet\LUX.WinTab.pas',
   LUX.WinTab.TabletFrame in '_LIBRARY\LUXOPHIA\LUX.PenTablet\LUX.WinTab.TabletFrame.pas' {TabletFrame: TFrame},
-  LUX.FMX.Pratform in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Pratform.pas';
+  LUX.FMX.Pratform in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Pratform.pas',
+  WINTAB in '_LIBRARY\LUXOPHIA\LUX.PenTablet\WINTAB.pas';
 
 {$R *.res}
 
