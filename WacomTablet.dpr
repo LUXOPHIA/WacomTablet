3d07717157a8f6bd7a24d08d0e24e3641bf8efe5
luxidea <contact@luxidea.net>
2020-10-02 00:09:23 +0900

    no message

--- WacomTablet.dpr
+++ WacomTablet.dpr
@@ -6,10 +6,10 @@ uses
   Main in 'Main.pas' {Form1},
   LUX.FMX.Messaging.Win in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Messaging.Win.pas',
   LUX.WinTab in '_LIBRARY\LUXOPHIA\LUX.PenTablet\LUX.WinTab.pas',
-  LUX.WinTab.TabletFrame in '_LIBRARY\LUXOPHIA\LUX.PenTablet\LUX.WinTab.TabletFrame.pas' {TabletFrame: TFrame},
   LUX.FMX.Pratform in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Pratform.pas',
   WINTAB in '_LIBRARY\LUXOPHIA\LUX.PenTablet\WINTAB.pas',
-  LUX in '_LIBRARY\LUXOPHIA\LUX\LUX.pas';
+  LUX in '_LIBRARY\LUXOPHIA\LUX\LUX.pas',
+  LUX.WinTab.TabletFrame in '_LIBRARY\LUXOPHIA\LUX.PenTablet\LUX.WinTab.TabletFrame.pas' {TabletFrame: TFrame};
 
 {$R *.res}
 
